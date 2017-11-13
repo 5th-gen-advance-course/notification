005fa878ef4611cba2d2cf8b9d407e7c343aac77
rathana-voy <rathanavoy07@gmail.com>
2017-11-13 10:13:08 +0700

    custome notification

--- app/src/main/java/com/example/ratha/notificationdemo/MainActivity.java
+++ app/src/main/java/com/example/ratha/notificationdemo/MainActivity.java
@@ -10,6 +10,7 @@ import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.RemoteViews;
 
 import butterknife.ButterKnife;
 import butterknife.OnClick;
@@ -30,18 +31,25 @@ public class MainActivity extends AppCompatActivity {
        postNitification();
     }
 
+    @OnClick(R.id.btnCustomNotification)
+    public void CustomNotification(View view){
+       customNotification();
+    }
+
+
     private void postNitification(){
         final String CHANEL_ID  ="my_chanel_01";
 
         NotificationCompat.Builder builder=new NotificationCompat.Builder(this,CHANEL_ID);
         builder.setSmallIcon(R.drawable.ic_teddy_bear_24dp);
         String text="You posted in your timeline";
+        int imageId=R.drawable.forest;
         builder.setContentTitle("A Im Posted");
         builder.setContentText(text);
 
         Intent mRestultIntent=new Intent(this,ResultActivity.class);
         mRestultIntent.putExtra("DATA",text);
-
+        mRestultIntent.putExtra("IMAGE",imageId);
 
         /*TaskStackBuilder taskStackBuilder=TaskStackBuilder.create(this);
         taskStackBuilder.addParentStack(ResultActivity.class);
@@ -63,7 +71,7 @@ public class MainActivity extends AppCompatActivity {
         //setBoxstyle
         NotificationCompat.InboxStyle inboxStyle=new NotificationCompat.InboxStyle();
         String[] events={   "1. Message for A Im",
-                            "2. Big View notificatio",
+                            "2. Big View notification",
                             "3. From HRD"};
 
         for(String s : events){
@@ -73,11 +81,11 @@ public class MainActivity extends AppCompatActivity {
 
         NotificationCompat.BigPictureStyle bigPictureStyle=new NotificationCompat.BigPictureStyle();
 
-        Bitmap bitmap=BitmapFactory.decodeResource(getResources(),R.drawable.ic_bear_460dp);
+        Bitmap bitmap=BitmapFactory.decodeResource(getResources(),R.drawable.forest);
         bigPictureStyle.setBigContentTitle("Big content title");
         bigPictureStyle.setSummaryText(getString(R.string.notification_big_pic_view_summary));
         bigPictureStyle.bigPicture(bitmap);
-        //bigPictureStyle.bigLargeIcon(bitmap);
+        bigPictureStyle.bigLargeIcon(bitmap);
         builder.setStyle(bigPictureStyle);
 
         builder.setTicker("some posted");
@@ -88,4 +96,31 @@ public class MainActivity extends AppCompatActivity {
         mNotificationManager.notify(mNotificationId,builder.build());
 
     }
+
+    public void customNotification(){
+
+        String CHANNEL_ID="my_channel_02";
+        NotificationCompat.Builder mBuilder=new NotificationCompat.Builder(this,CHANNEL_ID);
+        RemoteViews collapseRemoteViews =new RemoteViews(getPackageName(), R.layout.notification_layout);
+        RemoteViews expandedView =new RemoteViews(getPackageName(),R.layout.notification_expanded_layout);
+
+        Intent intent =new Intent(this,ResultActivity.class);
+
+        mBuilder.setSmallIcon(R.drawable.ic_bear_460dp);
+        mBuilder.setContentTitle("Chnana posted");
+        mBuilder.setContentText("Channa posted to AVA group");
+
+        mBuilder.setAutoCancel(true);
+        mBuilder.setContentIntent(PendingIntent.getActivity(this,0,intent,0));
+
+        mBuilder.setCustomContentView(collapseRemoteViews);
+        //mBuilder.setCustomBigContentView(expandedView)
+                //.setStyle(new NotificationCompat.DecoratedCustomViewStyle());
+
+        NotificationManager mNotificationManager= (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+
+        mNotificationManager.notify(100,mBuilder.build());
+
+    }
+
 }
--- app/src/main/java/com/example/ratha/notificationdemo/ResultActivity.java
+++ app/src/main/java/com/example/ratha/notificationdemo/ResultActivity.java
@@ -3,6 +3,7 @@ package com.example.ratha.notificationdemo;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import butterknife.BindView;
@@ -12,6 +13,8 @@ public class ResultActivity extends AppCompatActivity {
 
     @BindView(R.id.tvResult)
     TextView tvResult;
+    @BindView(R.id.image)
+    ImageView imageView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,7 +24,9 @@ public class ResultActivity extends AppCompatActivity {
         Intent intent=getIntent();
         if(null!=intent){
             String s=intent.getStringExtra("DATA");
+            int imageId= intent.getIntExtra("IMAGE" ,R.drawable.ic_bear_460dp);
             tvResult.setText(s);
+            imageView.setImageResource(imageId);
         }
     }
 }
